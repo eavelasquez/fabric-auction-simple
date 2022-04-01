5e52353c473812604baa77e1392c44dceaa0cc2f
Esteban Velasquez <eavc.30@gmail.com>
2022-04-01 13:04:00 -0500

    feat(back): add auction and bid structure types

---
+++ auction-chaincode/contract/auction.go
@@ -0,0 +1,14 @@
+package contract
+
+// Auction stores auction's data
+type Auction struct {
+	Type         string             `json:"objectType"`
+	ItemSold     string             `json:"item"`
+	Seller       string             `json:"seller"`
+	Orgs         []string           `json:"organizations"`
+	PrivateBids  map[string]BidHash `json:"privateBids"`
+	RevealedBids map[string]FullBid `json:"revealedBids"`
+	Winner       string             `json:"winner"`
+	Price        int                `json:"price"`
+	Status       string             `json:"status"`
+}
---
+++ auction-chaincode/contract/bid.go
@@ -0,0 +1,17 @@
+package contract
+
+// FullBid stores revealed bid's data
+type FullBid struct {
+	Type   string `json:"objectType"`
+	Price  int    `json:"price"`
+	Org    string `json:"organization"`
+	Bidder string `json:"bidder"`
+}
+
+// BidHash stores private bid's data
+type BidHash struct {
+	Org  string `json:"org"`
+	Hash string `json:"hash"`
+}
+
+const bidKeyType = "bid"
