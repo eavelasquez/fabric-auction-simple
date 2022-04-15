a8047d79a032962c12afa1dc72eaae1e6d14f436
Esteban Velasquez <eavc.30@gmail.com>
2022-04-15 07:49:36 -0500

    feat(back): add buildWallet function
    Add common code to build a wallet.

--- auction-application/src/utils/AppUtil.js
+++ auction-application/src/utils/AppUtil.js
@@ -37,3 +37,23 @@ exports.buildCCPOrg = (orgName) => {
   console.log(`Loaded the network configuration located at ${ccpPath}`);
   return ccp;
 };
+
+/**
+ * @description This function is used to create a new wallet.
+ * @param {*} Wallets - The Wallets class.
+ * @param {string} walletPath - Directory path to the wallet.
+ * @returns {*} The wallet object.
+ */
+exports.buildWallet = async (Wallets, walletPath) => {
+  // Create a new wallet, note that wallet is for managing identities.
+  let wallet;
+  if (walletPath) {
+    wallet = await Wallets.newFileSystemWallet(walletPath);
+    console.log(`Built a file system wallet at ${walletPath}`);
+  } else {
+    wallet = await Wallets.newInMemoryWallet();
+    console.log("Built an in memory wallet");
+  }
+
+  return wallet;
+};
