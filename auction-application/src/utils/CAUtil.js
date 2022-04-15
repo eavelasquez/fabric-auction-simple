7ff0b5176a50e22630fd3fbdda660e8a88143d1e
Esteban Velasquez <eavc.30@gmail.com>
2022-04-15 07:55:02 -0500

    feat(back): add buildCAClient function
    Add common code to build a certificate authority client.

--- auction-application/src/utils/CAUtil.js
+++ auction-application/src/utils/CAUtil.js
@@ -1,1 +1,22 @@
 "use strict";
+
+/**
+ * @description This function is used to build a new Fabric CA client.
+ * @param {*} FabricCAServices - The Fabric CA services class.
+ * @param {*} ccp - The common connection profile.
+ * @param {string} caHostName - The CA host name.
+ * @returns {*} The Fabric CA services object.
+ */
+exports.buildCAClient = (FabricCAServices, ccp, caHostName) => {
+  // Create a new CA client for interacting with the CA.
+  const caInfo = ccp.certificateAuthorities[caHostName]; // lookup CA details from config.
+  const caTLSCACerts = caInfo.tlsCACerts.pem;
+  const caClient = new FabricCAServices(
+    caInfo.url,
+    { trustedRoots: caTLSCACerts, verify: false },
+    caInfo.caName
+  );
+
+  console.log(`Built a CA Client named ${caInfo.caName}`);
+  return caClient;
+};
