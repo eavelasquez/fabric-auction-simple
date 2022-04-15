377a6a636f1c2bee89d5ccc7fb4a9c7033be7f95
Esteban Velasquez <eavc.30@gmail.com>
2022-04-15 07:57:04 -0500

    feat(back): add enrollAdmin function
    Add common code to enroll and import admin user into the wallet.

--- auction-application/src/utils/CAUtil.js
+++ auction-application/src/utils/CAUtil.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const adminUserId = "admin";
+const adminUserPasswd = "adminpw";
+
 /**
  * @description This function is used to build a new Fabric CA client.
  * @param {*} FabricCAServices - The Fabric CA services class.
@@ -19,4 +22,43 @@ exports.buildCAClient = (FabricCAServices, ccp, caHostName) => {
 
   console.log(`Built a CA Client named ${caInfo.caName}`);
   return caClient;
+};
+
+/**
+ *
+ * @param {*} caClient
+ * @param {*} wallet
+ * @param {*} orgMspId
+ */
+exports.enrollAdmin = async (caClient, wallet, orgMspId) => {
+  try {
+    // Check to see if we've already enrolled the admin user.
+    const identity = await wallet.get(adminUserId);
+    if (identity) {
+      console.log(
+        "An identity for the admin user already exists in the wallet"
+      );
+      return;
+    }
+
+    // Enroll the admin user, and import the new identity into the wallet.
+    const enrollment = await caClient.enroll({
+      enrollmentID: adminUserId,
+      enrollmentSecret: adminUserPasswd,
+    });
+    const x509Identity = {
+      credentials: {
+        certificate: enrollment.certificate,
+        privateKey: enrollment.key.toBytes(),
+      },
+      mspId: orgMspId,
+      type: "X.509",
+    };
+    await wallet.put(adminUserId, x509Identity);
+    console.log(
+      "Successfully enrolled admin user and imported it into the wallet"
+    );
+  } catch (error) {
+    console.error(`Failed to enroll admin user: ${error}`);
+  }
 };
